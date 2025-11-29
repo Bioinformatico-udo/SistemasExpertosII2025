7d06bff96274066d64e241be5fdb0e0a407d647c
pjdmartinez <pjdmartinez15.research@gmail.com>
2025-11-28 20:30:43 -0400

    estructura basica del proyecto.

---
+++ grupo4/app.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+import scikit-learn as kit
+
+
+
+"""
+Args:
+Returns:
+Raises:
+Example:
+https://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings
+"""
