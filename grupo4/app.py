07e673c312cb5f3c5282b01b6b382fd881617110
pjdmartinez <pjdmartinez15.research@gmail.com>
2025-11-30 17:49:19 -0400

    feat: elementos básicos para la interfaz

--- grupo4/app.py
+++ grupo4/app.py
@@ -1,7 +1,26 @@
-import customtkinter as ctk
-import scikit-learn as kit
+import customtkinter
+import sklearn as kit
 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        """datos iniciales de la aplicación"""
+        self.title("Inserte título de la aplicación")
+        self.geometry("400x150")
+        self.grid_columnconfigure((0,1), weight=1)
+        """datos de la estructura y organización de la aplicación
+        TODO: separar los atributos en clases conforme vaya avanzando la aplicación."""
+        self.button = customtkinter.CTkButton(self, text="botoncito", command=self.button_callback)
+        self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+    def button_callback(self):
+        print("¡Luces, cámara, acción!")
 
+app = App()
+app.mainloop()
 
 """
 Args:
