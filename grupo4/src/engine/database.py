5a2e899961722869de95a04e52aeedd456f1ad96
pjdmartinez <pjdmartinez15.research@gmail.com>
2026-02-06 21:10:44 -0400

    Update database.py

--- grupo4/src/engine/database.py
+++ grupo4/src/engine/database.py
@@ -1,9 +1,18 @@
 #aqui es donde va la conexión con la base de datos.
 from sqlalchemy import create_engine
-db_connection = create_engine("mysql+mysqldb://admin:password@localhost:3306/expertsystems",
+db_connection = create_engine("mysql+mysqldb://root:-@localhost:3306/expertsystems",
     pool_recycle=3000, echo=True
 )
 
 #Hacer clases
 #Database connection
+#Hacer CRUD de las especies de porcelanidos.
 #
+#
+
+class DB:
+    def __init__(self, user, password, url, port):
+        self.user = user
+        self.password = password
+        self.url = url
+        self.port = port
