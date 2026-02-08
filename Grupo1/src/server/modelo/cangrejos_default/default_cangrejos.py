cd8b0f95c93c8cf1b501aa2ff536318014ea0db3
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2026-02-08 00:42:18 -0400

    colocado segundo plano en default

--- Grupo1/src/server/modelo/cangrejos_default/default_cangrejos.py
+++ Grupo1/src/server/modelo/cangrejos_default/default_cangrejos.py
@@ -1,8 +1,7 @@
 
-
-from data.sync_csv_json.sync_csv import sync_csv_json
+from data.Corutine_training_sync.courutine_training import start_training_thread
 
 #funcion para retornar el csv actualizado con los cangrejos por defecto, esta funcion se llama desde el endpoint /cangrejos_default para actualizar el csv con los cangrejos por defecto cada vez que se accede a ese endpoint
 def default_cangrejos():
-    sync_csv_json('modelo/cangrejos_default/default_cangrejos.json')
+    start_training_thread()
     return {"mensaje":"¡Éxito! CSV actualizado con los cangrejos por defecto."}
